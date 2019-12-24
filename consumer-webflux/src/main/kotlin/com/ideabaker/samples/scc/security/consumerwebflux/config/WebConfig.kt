b310b4e72fb3cf1ffe5f108fe34b4a1b9a01436b
Arthur Kazemi <bidadh@gmail.com>
2019-12-25 00:33:30 +1100

    cleanup

--- consumer-webflux/src/main/kotlin/com/ideabaker/samples/scc/security/consumerwebflux/config/WebConfig.kt
+++ consumer-webflux/src/main/kotlin/com/ideabaker/samples/scc/security/consumerwebflux/config/WebConfig.kt
@@ -1,15 +1,9 @@
 package com.ideabaker.samples.scc.security.consumerwebflux.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
-import org.springframework.http.codec.ServerCodecConfigurer
-import org.springframework.http.codec.json.Jackson2JsonDecoder
-import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
@@ -24,16 +18,6 @@ import org.springframework.web.reactive.function.client.WebClient
 @Configuration
 class WebConfig : WebFluxConfigurer {
   private val logger = LoggerFactory.getLogger(WebConfig::class.java)
-
-  @Autowired
-  lateinit var objectMapper: ObjectMapper
-
-  override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-    configurer.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper))
-    configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
-    configurer.defaultCodecs().enableLoggingRequestDetails(true)
-  }
 
   @Bean
   fun webClientBuilder(): WebClient.Builder {
